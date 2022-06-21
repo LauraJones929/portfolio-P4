b2c29e0b9edd9c03ad7519f8ddcacce4750eb6b9
LauraJones929 <lauraljones19@gmail.com>
2022-06-21 20:33:11 +0000

    Add product details functionality and style memberships page

--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
     """
-    A function to show gym sessions and memberships
+    A function to show all memberships
     """
 
     products = Product.objects.all()
@@ -15,3 +15,16 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A function to show individual memberships
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
