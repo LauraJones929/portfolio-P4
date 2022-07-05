3b2288e61a7947b5f0158336aa6485e6152ca263
LauraJones929 <lauraljones19@gmail.com>
2022-07-05 19:05:08 +0000

    Delete membership functionality completed

--- products/views.py
+++ products/views.py
@@ -50,11 +50,12 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added membership!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add membership. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to add membership. Please ensure the form is valid.')
     else:
         form = ProductForm()
         
@@ -90,3 +91,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete a membership
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Membership deleted!')
+    return redirect(reverse('products'))
