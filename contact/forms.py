84f241ece44af540b72cb1b30d536ec2f971361b
LauraJones929 <lauraljones19@gmail.com>
2022-07-05 20:03:11 +0000

    Add contact template

---
+++ contact/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.Form):
+    """
+    Contact form to collect user queries
+    """
+
+    # class Meta:
+    #     """ Set fields from Contact model """
+    #     model = Contact
+    #     fields = ('name', 'email', 'subject', 'message',)
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(widget=forms.EmailInput(), label='Email Address')
+    subject = forms.CharField(label='Subject', max_length=100)
+    message = forms.CharField(label='Message', max_length=300, widget=forms.Textarea())
