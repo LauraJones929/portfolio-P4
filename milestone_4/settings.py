1eac2f51e0ff53b84acb57b66ac9d67b1668f534
LauraJones929 <lauraljones19@gmail.com>
2022-07-11 20:15:06 +0000

    Removed secret key and set debug

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-)+qix&7iop&z)iv+)wybvxb0e%ij7x-uh_g+r0z(8eo&vkasvc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['project-4-ljfitness.herokuapp.com', 'localhost']
 
