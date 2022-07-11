b93effdb53c9ffaa626bd2c97bbafcd0844d10f4
LauraJones929 <lauraljones19@gmail.com>
2022-07-11 17:59:16 +0000

    Add Allowed Hosts to settings file

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)+qix&7iop&z)iv+)wybvxb0e%ij7x-uh_g+r0z(8eo&vkasvc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-4-ljfitness.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
